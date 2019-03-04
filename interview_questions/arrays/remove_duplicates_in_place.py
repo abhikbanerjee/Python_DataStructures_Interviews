b986a9bc25d3184c4f9c27219e371e9bfe059fd4
abhik_banerjee <abhik.banerjee@oracle.com>
2019-03-03 21:37:22 -0800

    removing duplicates in place

---
+++ interview_questions/arrays/remove_duplicates_in_place.py
@@ -0,0 +1,23 @@
+
+
+def remove_duplicates(nums):
+	if len(nums) == 0:
+		return []
+	i, write_index = 1,1
+	while i < len(nums)-1:
+		if nums[i] != nums[i-1]:
+			nums[write_index] = nums[i]
+			write_index += 1
+		i += 1
+	return write_index-1
+
+def print_arr(arr, index):
+	for i in range(0, index):
+		print(arr[i])
+
+def main():
+	ar1 = [1,1,2,2,3,4,5,5]
+	print(print_arr(ar1, remove_duplicates(ar1)))
+
+if __name__ == '__main__':
+	main()
