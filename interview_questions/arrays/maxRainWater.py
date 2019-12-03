0be9dc01b5352e1e0b4d7f540ebcf755ba5486bf
abhik_banerjee <abhik.banerjee@oracle.com>
2019-12-02 21:02:38 -0800

    rain water capture

---
+++ interview_questions/arrays/maxRainWater.py
@@ -0,0 +1,16 @@
+from typing import List
+
+def maxArea(height: List[int]) -> int:
+	area, l, r = 0, 0, len(height) - 1
+	while l < r:
+		area = max(area, min(height[l], height[r]) * (r - l))
+
+		if height[l] < height[r]:
+			l += 1
+		else:
+			r -= 1
+	return area
+
+ht = [1,8,6,2,5,4,8,3,7]
+print(maxArea(ht))
+
