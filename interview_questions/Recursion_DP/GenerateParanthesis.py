9a2367d73fda2a69e3bb311e7892c94e772f5ff7
Abhik Banerjee <banerjee.abhik.hcl@gmail.com>
2020-06-02 18:15:50 -0700

    generate Paranthesis problem

---
+++ interview_questions/Recursion_DP/GenerateParanthesis.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+def generateParenthesis(n: int) -> List[str]:
+	def gen_paran_helper(current_str: str, final_list: List[str], open_count, close_count, max_str):
+		if len(current_str) == max_str * 2:
+			final_list.append(current_str)
+			return
+		if open_count < max_str:
+			gen_paran_helper(current_str + "(", final_list, open_count + 1, close_count, max_str)
+		if close_count < open_count:
+			gen_paran_helper(current_str + ")", final_list, open_count, close_count + 1, max_str)
+
+	# return final_list
+
+	final_list = []
+	gen_paran_helper("", final_list, 0, 0, n)
+	return final_list
+
+
+print(generateParenthesis(3))
