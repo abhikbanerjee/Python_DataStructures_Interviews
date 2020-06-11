bacf4384699bda488b5494742a1c7c76d016c9d4
Abhik Banerjee <banerjee.abhik.hcl@gmail.com>
2020-06-11 07:41:53 -0700

    robot unique paths

---
+++ interview_questions/Recursion_DP/RobotUniquePath.py
@@ -0,0 +1,27 @@
+
+def robot_unique_paths(m: int, n: int) -> int:
+	if m < 1 or n < 1:
+		return 0
+	# matrix to store the dp numbers
+	result_dp = [[0 for _ in range(0, n)] for _ in range(0, m)]
+
+	# fill the 1st row and the first column
+	for i in range(0, n):
+		result_dp[0][i] = 1
+
+	for i in range(0, m):
+		result_dp[i][0] = 1
+
+	# the cell can have the counts from left or top unique ways
+	for i in range(1, m):
+		for j in range(1, n):
+			result_dp[i][j] = result_dp[i - 1][j] + result_dp[i][j - 1]
+
+	return result_dp[m - 1][n - 1]
+
+
+print("Unique paths for a 3 X 2 grid from Start to Finish - ",robot_unique_paths(3,2))
+print(" ")
+print("Unique paths for a 7 X 3 grid from Start to Finish - ",robot_unique_paths(7,3))
+
+
