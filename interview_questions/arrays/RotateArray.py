7c4ad1868b70cd8fcb5b4af12742789b5d3350b7
Abhik Banerjee <banerjee.abhik.hcl@gmail.com>
2020-06-20 11:50:56 -0700

    rotate array problem

---
+++ interview_questions/arrays/RotateArray.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+# Rotate Array k times - LC:189
+def rotate_array(nums: List[int], k: int) -> None:
+	"""
+	Do not return anything, modify nums in-place instead.
+	"""
+	if k == 0 or len(nums) < 1:
+		return
+	k = k% len(nums)
+
+	def reverse(nums: List[int], start: int, end: int):
+		while start <= end:
+			nums[start], nums[end] = nums[end], nums[start]
+			start += 1
+			end -= 1
+
+	reverse(nums, 0, len(nums) - 1)
+	reverse(nums, 0, k-1)
+	reverse(nums, k, len(nums) - 1)
+
+
+arr = [1,2,3,4,5,6,7]
+k = 3
+rotate_array(arr,k)
+print(arr)
